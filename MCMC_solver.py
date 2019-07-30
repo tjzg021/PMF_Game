cc8c66697b34fc3481cc02c712229e2e97f7c03e
tjzg021 <38375373+tjzg021@users.noreply.github.com>
2019-07-30 15:36:26 +0800

    Update MCMC_solver.py
    update for compute PMF

--- MCMC_solver.py
+++ MCMC_solver.py
@@ -1,26 +1,53 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
+
+import numpy as np
 import GenerateRandomData as GenData
 
+from collections import Counter
+
+
 def PMF_solver(): #data, pid, teams_num):
+
     
+
     Data = GenData.GenerateRandomData(123)
+
     teams_num = 30
-    
+
     data = Data
 
-    #X1=np.random.randn(1000)
-    X1 = range(1,31)
-    fig1,ax1 = plt.subplots(1, 1, figsize=(10,4));
-    ax1.scatter(X1, Data);ax1.set_xlabel('X1');ax1.set_ylabel('Y'); 
-    
+
+
+
     total = len(data)
+    Counter(data)
     print("len: %d" %(total))
+
     pmf = {}
+
     for i in range(1,teams_num+1) :
         pmf[i]=[]
-        pmf[i].append([data.count(i)/float(total)])
+        print ( "i= %d    %d times" %(i,sum(data == i)) )
+
+    Y = []
+    for i in range(1,teams_num+1) :
+        print ("PMF for %d   %f" %(i,pmf[i]))
+        Y.append(pmf[i])
+
+
+    X1 = range(1, 31)
+    print (X1)
+    print (len(Y),Y)
+    fig1, ax1 = plt.subplots(1, 1, figsize=(10, 4))
+
+    ax1.scatter(X1,Y)
+    ax1.set_xlabel('X1')
+    ax1.set_ylabel('Y')
+    plt.show()
     
-    
+    # plt.hist(data,range=(1,30))
+    # plt.show()
+    return pmf
+
 PMF_solver()
