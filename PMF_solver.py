89fbcc34016c7b8a7755cc7a9677ab114ae2476c
tjzg021 <38375373+tjzg021@users.noreply.github.com>
2019-08-22 02:21:01 +0800

    Update PMF_solver.py
    change to only caculate 50 datas

--- PMF_solver.py
+++ PMF_solver.py
@@ -25,25 +25,21 @@ def PMF_solver(data, teams_num):
 
         Y.append(pmf[i])
 
-    ################################show figure
-
-    #    X1 = range(1, teams_num+1)
-
-    #    print (X1)
-
-    #    print (len(Y),Y)
-
-    #    fig1, ax1 = plt.subplots(1, 1, figsize=(10, 4))
-
-    #
-
-    #    ax1.scatter(X1,Y)
-
-    #    ax1.set_xlabel('X1')
-
-    #    ax1.set_ylabel('Y')
-
-    #    plt.show()
+#    ################################show figure
+#    X1 = range(1, teams_num+2)
+#
+#    print (X1)
+#
+#    print (len(Y),Y)
+#
+#    fig1, ax1 = plt.subplots(1, 1, figsize=(10, 4))
+#
+#    
+#
+#    ax1.scatter(X1,Y)
+#    ax1.set_xlabel('X1')
+#    ax1.set_ylabel('Y')
+#    plt.show()
 
 
 
@@ -56,6 +52,7 @@ def UpdatePMF(R, Output, Iteration, TotalRobot):
     for i in range(0, TotalRobot):
         R[i].DATA[Iteration] = Output[i]
 
-    if Iteration >=500:
+    if Iteration >=50:
         for i in range(0, TotalRobot ):
-                R[i].PMF = PMF_solver(list(R[i].DATA.values()), TotalRobot)
+                #R[i].PMF = PMF_solver(list(R[i].DATA.values()), TotalRobot)
+                R[i].PMF = PMF_solver(list(R[i].DATA.values())[Iteration-50:Iteration], TotalRobot)
