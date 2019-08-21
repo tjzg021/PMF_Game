bf3ed5853eb2740030207778a6e0b097d68d8a09
tjzg021 <38375373+tjzg021@users.noreply.github.com>
2019-08-21 21:53:22 +0800

    Update PMF_solver.py

--- PMF_solver.py
+++ PMF_solver.py
@@ -1,42 +1,61 @@
 import numpy as np
+
 import matplotlib.pyplot as plt
+
 from collections import Counter
 
 
 ##################################to caculate PMF of input data
-def PMF_solver(data, teams_num):
 
+def PMF_solver(data, teams_num):
     total = len(data)
+
     datacounter = Counter(data)
-    #print("len: %d" %(total))
+
+    # print("len: %d" %(total))
+
+
 
     pmf = {}
+
     Y = []
-    for i in range(0,teams_num) :
-        pmf[i] = datacounter[i+1]/total
+
+    for i in range(0, teams_num+1):
+        pmf[i] = datacounter[i + 1] / float(total)
+
         Y.append(pmf[i])
-        
-################################show figure
-#    X1 = range(1, teams_num+1)
-#    print (X1)
-#    print (len(Y),Y)
-#    fig1, ax1 = plt.subplots(1, 1, figsize=(10, 4))
-#
-#    ax1.scatter(X1,Y)
-#    ax1.set_xlabel('X1')
-#    ax1.set_ylabel('Y')
-#    plt.show()
-    
+
+    ################################show figure
+
+    #    X1 = range(1, teams_num+1)
+
+    #    print (X1)
+
+    #    print (len(Y),Y)
+
+    #    fig1, ax1 = plt.subplots(1, 1, figsize=(10, 4))
+
+    #
+
+    #    ax1.scatter(X1,Y)
+
+    #    ax1.set_xlabel('X1')
+
+    #    ax1.set_ylabel('Y')
+
+    #    plt.show()
+
+
+
     return pmf
 
+
 ##################################update PMF every round
+
 def UpdatePMF(R, Output, Iteration, TotalRobot):
-
-    for i in range(0,TotalRobot-1):
+    for i in range(0, TotalRobot):
         R[i].DATA[Iteration] = Output[i]
-        
-    for i in range(0,TotalRobot-1):
-        for j in range(0,TotalRobot):
-            R[i].PMF[j] = PMF_solver(  R[i].DATA.values() , TotalRobot)
 
-
+    if Iteration >=500:
+        for i in range(0, TotalRobot ):
+                R[i].PMF = PMF_solver(list(R[i].DATA.values()), TotalRobot)
